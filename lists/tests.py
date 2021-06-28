6a9af6716816bde320603d3d5ed47e2ffd48ac10
Siarhei Shveadko <siarhei_shveadko@epam.com>
2021-06-28 14:05:40 +0300

    refactor home_page

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve  # джанго использует для преобразования url-адреса и нахождения функций представления, в соответствии которым они должны быть поставлены
 from .views import home_page
+from django.template.loader import render_to_string  # преобразование шаблона в HTML-разметку
 
 
 # class SmokeTest(TestCase):
@@ -12,14 +13,19 @@ from .views import home_page
 
 class HomePageTest(TestCase):
     """test home page"""
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve("/")
-        self.assertEqual(found.func, home_page)
+    # def test_root_url_resolves_to_home_page_view(self):
+    #     found = resolve("/")
+    #     self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()  # создаем объект HttpRequest(то, что увидит Djangо, когда браузер пользователя запросит страницу
-        response = home_page(request)
-        html = response.content.decode("utf-8")  # Извлекаем содержимое отклика(это необработанные байты), вызываем decode для конвертации в html
-        self.assertTrue(html.startswith("<html>"))
-        self.assertIn("<title>To-Do lists</title", html)
-        self.assertTrue(html.endswith("</html>"))
+    # def test_home_page_returns_correct_html(self):
+    #     request = HttpRequest()  # создаем объект HttpRequest(то, что увидит Djangо, когда браузер пользователя запросит страницу
+    #     response = home_page(request)
+    #     html = response.content.decode("utf8")  # Извлекаем содержимое отклика(это необработанные байты), вызываем decode для конвертации в html
+    #     expected_html = render_to_string("home.html")
+    #     self.assertEqual(html, expected_html)
+
+    def test_home_page_returns_correct_html_with_django_cliend(self):
+        response = self.client.get("/")  # вместио создания HttpRequest и вызовы вьюхи
+        html = response.content.decode("utf8")
+
+        self.assertTemplateUsed(response, "home.html")
