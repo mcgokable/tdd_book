107a4e09276754f6578eeb744bb526d22d549f46
Siarhei Shveadko <siarhei_shveadko@epam.com>
2021-06-28 11:04:48 +0300

    First module test and url, fake view

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,16 @@
 from django.test import TestCase
+from django.urls import resolve  # джанго использует для преобразования url-адреса и нахождения функций представления, в соответствии которым они должны быть поставлены
+from .views import home_page
 
 
-class SmokeTest(TestCase):
-    """smoke test"""
-    def test_bad_maths(self):
-        """wrong maths calculate"""
-        self.assertEqual(1 + 1, 3)
+# class SmokeTest(TestCase):
+#     """smoke test"""
+#     def test_bad_maths(self):
+#         """wrong maths calculate"""
+#         self.assertEqual(1 + 1, 3)
+
+class HomePageTest(TestCase):
+    """test home page"""
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
