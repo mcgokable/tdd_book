6b5deda1421aba668dc20007bb9bb2b0598694d3
Siarhei Shveadko <siarhei_shveadko@epam.com>
2021-06-28 12:58:03 +0300

    Func test gives possibility to enter element of list

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve  # джанго использует для преобразования url-адреса и нахождения функций представления, в соответствии которым они должны быть поставлены
 from .views import home_page
@@ -14,3 +15,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve("/")
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  # создаем объект HttpRequest(то, что увидит Djangо, когда браузер пользователя запросит страницу
+        response = home_page(request)
+        html = response.content.decode("utf-8")  # Извлекаем содержимое отклика(это необработанные байты), вызываем decode для конвертации в html
+        self.assertTrue(html.startswith("<html>"))
+        self.assertIn("<title>To-Do lists</title", html)
+        self.assertTrue(html.endswith("</html>"))
--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,5 +1,9 @@
+import time
+
 from selenium import webdriver
 import unittest
+
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,7 +20,22 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         """We can start list and receive it later"""
         self.browser.get("http://localhost:8000")
+
+        header_text = self.browser.find_element_by_tag_name("h1").text
         self.assertIn("To-Do", self.browser.title)
+
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
+
+        inputbox.send_keys("Buy Mark Lutz book.")  # ввод данных в поля ввода input
+        inputbox.send_keys(Keys.ENTER)  # отправляем спец.клавишу  ENTER
+        time.sleep()  # выше обновляем страницу, это гарантирует, что она загрузится
+
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(any(row.text == "1: Buy Mark Lutz book." for row in rows))
+
+
         self.fail("Finish test")  # икогда не срабатывает и генерирует переданное сообщение об ошибке
 
 
