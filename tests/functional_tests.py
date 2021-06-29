70b3946b0e331561e840e04e96e74891b2b15f60
Siarhei Shveadko <siarhei_shveadko@epam.com>
2021-06-29 12:18:00 +0300

    Add Item model

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -17,6 +17,12 @@ class NewVisitorTest(unittest.TestCase):
         """cleaning"""
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """подтверждение строки в таблице списка"""
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """We can start list and receive it later"""
         self.browser.get("http://localhost:8000")
@@ -31,11 +37,14 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)  # отправляем спец.клавишу  ENTER
         time.sleep(1)  # выше обновляем страницу, это гарантирует, что она загрузится
 
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        # self.assertTrue(any(row.text == "1: Buy Mark Lutz book." for row in rows), f"Новый элемент списка не появился в таблице. Содержимым было:\n{table.text}")
-        self.assertIn('1: Buy Mark Lutz book.', [row.text for row in rows])
-        self.assertIn('2: Buy Mark Lutz book tom 2.', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy Mark Lutz book.')
+        self.check_for_row_in_list_table('2: Buy Mark Lutz book tom 2.')
+
+        # table = self.browser.find_element_by_id("id_list_table")
+        # rows = table.find_elements_by_tag_name("tr")
+        # # self.assertTrue(any(row.text == "1: Buy Mark Lutz book." for row in rows), f"Новый элемент списка не появился в таблице. Содержимым было:\n{table.text}")
+        # self.assertIn('1: Buy Mark Lutz book.', [row.text for row in rows])
+        # self.assertIn('2: Buy Mark Lutz book tom 2.', [row.text for row in rows])
 
         self.fail("Finish test")  # никогда не срабатывает и генерирует переданное сообщение об ошибке
 
