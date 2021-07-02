db392cefe1a1fc2a2af5c4fdeffed80a63e2b2eb
Siarhei Shveadko <siarhei_shveadko@epam.com>
2021-07-02 10:12:54 +0300

    First step for maket and css

--- tests/functional_tests/tests.py
+++ tests/functional_tests/tests.py
@@ -117,6 +117,22 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy Mark Lutz book.', page_text)
         self.assertIn('Buy milk', page_text)
 
+    def test_layout_and_styling(self):
+        """тест макета и стилевого оформления"""
+#         Эдит открывает домашнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Она замечает, что поле ввода аккуратно центрировано
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
+
+        # Она начинает новый список и видит, что поле ввода тоже центрировано
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
 
 # if __name__ == "__main__":
 #     unittest.main(warnings="ignore")  # запускает исполнителя тестов, unittest , а он автоматически найдет в файле тест.классы, методы и выполнит их. warnings подавляет лишние предупреждающие сообщения Resourcewarning
